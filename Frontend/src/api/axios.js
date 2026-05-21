243f8d2834240b1c21e8c1d284a10a11e544d24f
Vitaliy Nesterenko <vitaliyviktorovich123@gmail.com>
2026-05-21 12:10:35 +0200

    fix api url

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://server_docker:5000/api'
+    baseURL: 'http://3.120.224.76:5000/api'
 });
 
 api.interceptors.request.use((config) => {
